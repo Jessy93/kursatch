b8a408ef8d5f25cdf8a8aa02eb98507c6c8e3835
Ckam <ckam225@gmail.com>
2021-12-19 18:48:36 +0300

    clean

--- python/main.py
+++ python/main.py
@@ -3,22 +3,28 @@ from typing import Optional
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import pysftp
+from aes.aes import AESCrypt
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/models/upload")
 async def upload(file: UploadFile = File(...)):
     print(file.filename)
-    with open("media/"+file.filename, "wb") as buffer:
+    path = "media/"+file.filename
+
+    with open(path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+        encryted_path = AESCrypt().encrypt(path, '000')
         with pysftp.Connection('192.168.1.6', username='macbook', password='Jessy93') as sftp:
-          print("connected")
-          with sftp.cd("/Users/macbook/Desktop/media-users/"): # temporarily chdir to public
-              print("ready to upload")
-              sftp.put("media/"+file.filename) # upload file to public/ on remote
-              print("file successfully upload")
+            print("connected")
+            with sftp.cd("/Users/macbook/Desktop/media-users/"):
+                print("ready to upload")
+                sftp.put(encryted_path)
+                print("file successfully upload")
     return {"filename": file.filename}
