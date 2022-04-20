1ed9ad4cc5646db698edfab1a7bbd4f50fd6bdbb
Jessy93 <adiatu.ibrahim@gmail.com>
2022-04-20 14:42:03 +0400

    Cleaning project

---  vue.config.js
+++  vue.config.js
@@ -3,6 +3,7 @@ const webpack  = require('webpack');
 module.exports = {
   devServer: {
     proxy: 'rtsp://91.222.130.82:8554',
+    proxy: 'https://raw.githubusercontent.com/phoboslab/jsmpeg/master/jsmpeg.min.js',
     proxy: 'http://localhost:5000'
   },
   publicPath: process.env.NODE_ENV === 'production'
--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,6 @@
 </template>
 
 <script>
-import hls from "./utils/hls"
-import EmptyLayout from "@/layouts/EmptyLayout";
-import MainLayout from "@/layouts/MainLayout";
-
-
 export default {
   // rtsp://91.222.130.82:8554/ch1
   computed: {
@@ -23,20 +18,9 @@ export default {
     EmptyLayout,
     MainLayout,
   },
-  // mounted() {
-  //   // HLS First  option Doesnt work-->
-  //   if (document.getElementById('hls-coding')) {
-  //     return;} else {
-  //     var scriptTag = document.createElement("script");
-  //     scriptTag.src = hls;
-  //     scriptTag.id = "hls-coding";
-  //     document.getElementsByTagName('head')[0].appendChild(scriptTag);
-  //   }
-  // }
 }
 
 </script>
-
 
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
